657bc94a0336c683606466c6a538fd7b72464a65
tejakammineni204 <kammineni.teja@gmail.com>
2025-05-12 11:45:26 -0400

    Substraction.java

---
+++ src/demo/Substraction.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class Substraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a= 20;
+		int b=10;
+		System.out.println("Substration of a and b is "+ (a-b));
+
+	}
+
+}
