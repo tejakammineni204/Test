a4b091885e8474b8ce3cb4f33d3fde19c40983c1
tejakammineni204 <kammineni.teja@gmail.com>
2025-05-12 11:37:50 -0400

    Multiplication.java

---
+++ src/demo/Multiplication.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a=29;
+        int b=1;
+        System.out.println("Multiplication is"+ a*b);
+	}
+
+}
