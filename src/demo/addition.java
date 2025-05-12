e735dc51e0ed5ce00d54cf2d2ee5613f3a3676f1
tejakammineni204 <kammineni.teja@gmail.com>
2025-05-12 11:30:39 -0400

    ADDed the project

---
+++ src/demo/addition.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Au
+		int a=15;
+		int b=3;
+		System.out.println("division of a and b is "+ a/b);
+	
+
+	}
+
+}
